b3a6d4a98292870602e2aedc6a6e3a6782da2dcd
Felix <felixsummerer@freenet.de>
2024-10-22 12:24:33 +0200

    erste hausaufgabe

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -7,35 +7,52 @@ export class Name {
     private delimiter: string = this.DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+       this.components = other
+       if (delimiter != undefined){
+        this.delimiter = delimiter
+       }
     }
 
     public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        let name: string = ''
+
+        this.components.forEach(element => {
+            if (name.length == 0){
+                name += element
+            }else{
+            name += delimiter
+            name += element
+            }
+        });
+
+        return name
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        if (i >= 0 && i < this.components.length) {
+            return this.components[i];
+        }
+        return "";
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.components.splice(i,1,c)
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.components.splice(i,0,c)
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components.push(c)
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        this.components.splice(i,1)
     }
 
 }
