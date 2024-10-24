82273560dae6ac105d38021db6205f6d268d1512
Nicolas <nicolas@linuxstar.net>
2024-10-24 20:55:55 +0200

    Implemented name class

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -1,41 +1,61 @@
 export class Name {
+  public readonly DEFAULT_DELIMITER: string = ".";
+  private readonly ESCAPE_CHARACTER = "\\";
 
-    public readonly DEFAULT_DELIMITER: string = '.';
-    private readonly ESCAPE_CHARACTER = '\\';
+  private components: string[] = [];
+  private delimiter: string = this.DEFAULT_DELIMITER;
 
-    private components: string[] = [];
-    private delimiter: string = this.DEFAULT_DELIMITER;
-
-    constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+  constructor(other: string[], delimiter?: string) {
+    this.components = other;
+    if (delimiter) {
+      this.delimiter = delimiter;
     }
+  }
 
-    public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+  // @methodtype conversion-method
+  public asNameString(delimiter: string = this.delimiter): string {
+    let string = "";
+    for (let i = 0; i < this.components.length; i++) {
+      if (i === this.components.length - 1) {
+        string = string + this.components[i];
+      } else {
+        string = string + this.components[i] + delimiter;
+      }
     }
+    return string;
+  }
 
-    public getComponent(i: number): string {
-        throw new Error("needs implementation");
+  // @methodtype get-method
+  public getComponent(i: number): string {
+    return this.components[i];
+  }
+
+  // @methodtype set-method
+  public setComponent(i: number, c: string): void {
+    this.components[i] = c;
+  }
+
+  // @methodtype get-method
+  public getNoComponents(): number {
+    return this.components.length;
+  }
+
+  // @methodtype command-method
+  public insert(i: number, c: string): void {
+    if (i >= 0 && i <= this.components.length) {
+      this.components.splice(i, 0, c);
     }
+  }
 
-    public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+  // @methodtype command-method
+  public append(c: string): void {
+    this.components.push(c);
+  }
+
+  // @methodtype command-method
+  public remove(i: number): void {
+    if (i >= 0 && i < this.components.length) {
+      this.components.splice(i, 1);
     }
-
-    public getNoComponents(): number {
-        throw new Error("needs implementation");
-    }
-
-    public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
-    }
-
-    public append(c: string): void {
-        throw new Error("needs implementation");
-    }
-
-    public remove(i: number): void {
-        throw new Error("needs implementation");
-    }
-
+  }
 }
