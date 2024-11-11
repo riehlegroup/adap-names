ec37df0de138a270f08362b83ca4f120815fb18d
joluj <johannes.lukas.jablonski@gmail.com>
2024-11-11 09:59:37 +0100

    Add missing method stubs

--- src/adap-b03/names/StringArrayName.ts
+++ src/adap-b03/names/StringArrayName.ts
@@ -6,7 +6,28 @@ export class StringArrayName extends AbstractName {
     protected components: string[] = [];
 
     constructor(other: string[], delimiter?: string) {
+        super();
         throw new Error("needs implementation");
     }
 
+    getNoComponents(): number {
+        throw new Error("needs implementation");
+    }
+
+    getComponent(i: number): string {
+        throw new Error("needs implementation");
+    }
+    setComponent(i: number, c: string) {
+        throw new Error("needs implementation");
+    }
+
+    insert(i: number, c: string) {
+        throw new Error("needs implementation");
+    }
+    append(c: string) {
+        throw new Error("needs implementation");
+    }
+    remove(i: number) {
+        throw new Error("needs implementation");
+    }
 }
--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -7,7 +7,28 @@ export class StringName extends AbstractName {
     protected length: number = 0;
 
     constructor(other: string, delimiter?: string) {
+        super();
         throw new Error("needs implementation");
     }
 
+    getNoComponents(): number {
+        throw new Error("needs implementation");
+    }
+
+    getComponent(i: number): string {
+        throw new Error("needs implementation");
+    }
+    setComponent(i: number, c: string) {
+        throw new Error("needs implementation");
+    }
+
+    insert(i: number, c: string) {
+        throw new Error("needs implementation");
+    }
+    append(c: string) {
+        throw new Error("needs implementation");
+    }
+    remove(i: number) {
+        throw new Error("needs implementation");
+    }
 }
