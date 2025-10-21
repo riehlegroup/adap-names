470f98c2d7d0443ee1113324b4de592cb81e2e2d
Dirk Riehle <dirk@riehle.org>
2025-10-21 17:50:01 +0200

    Replaced control character with special character

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -24,8 +24,8 @@ export class Name {
     }
 
     /**
-     * Returns a human-readable representation of the Name instance using user-set control characters
-     * Control characters are not escaped (creating a human-readable string)
+     * Returns a human-readable representation of the Name instance using user-set special characters
+     * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
     public asString(delimiter: string = this.delimiter): string {
@@ -33,9 +33,9 @@ export class Name {
     }
 
     /** 
-     * Returns a machine-readable representation of Name instance using default control characters
+     * Returns a machine-readable representation of Name instance using default special characters
      * Machine-readable means that from a data string, a Name can be parsed back in
-     * The control characters in the data string are the default characters
+     * The special characters in the data string are the default characters
      */
     public asDataString(): string {
         throw new Error("needs implementation or deletion");
